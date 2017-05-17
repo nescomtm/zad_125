8ed7e5c582972ab07df7ec7d2b5d935ef3c5c1db
DELL-M4800\TM <tomczyk@nescom.co>
2017-05-17 21:25:29 +0200

    zad 12_5 ver1

---
+++ js/script.js
@@ -0,0 +1,68 @@
+
+
+var url = 'http://api.icndb.com/jokes/random';
+
+var $button = $('#get-joke').click(function() {
+	getJoke();
+});
+
+var $paragraph = $('#joke');
+
+function getJoke() {
+    $.ajax({
+        method: 'GET',
+        url: url, //tutaj dziwna konstrukcja, ale po lewej stronie mamy nazwę parametru, a po prawej jest nazwa zmiennej przechowującej wartość
+        success: function(res) {
+            $paragraph.text(res.value.joke);
+        }
+
+    });
+}
+
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+
+function getQuote() {
+    $.getJSON(quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+        getQuote();
+    }
+
+    if (tweetText.length > 140) {
+        getQuote();
+    } else {
+        var tweet = tweetLink + encodeURIComponent(tweetText);
+        $('.quote').text(quoteText);
+        $('.author').text("Author: " + quoteAuthor);
+        $('.tweet').attr('href', tweet);
+    }
+$('.tweet').attr('href', tweet);
+}
+
+
+
+//$('.tweet').attr('href', tweet);
+
+
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
+
